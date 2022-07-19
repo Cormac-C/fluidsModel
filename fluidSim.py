5942f6489bad98e85e5163593df9c981ba930f54
Cormac Cureton <cormaccureton@gmail.com>
2022-07-19 16:21:56 -0400

    Add in losses

--- fluidSim.py
+++ fluidSim.py
@@ -1,24 +1,37 @@
 import math
+import datetime
 
-#All values in meters
+# All values in meters
 tankArea = 0.32 * 0.36
-pipeArea = math.pi / 4 * 0.00794 ** 2
+pipeDiameter = 0.00794
+pipeArea = math.pi / 4 * pipeDiameter ** 2
 gravity = 9.81
 
+def findLosses (length, velocity):
+    # Friction factor wrong
+    frictionFactor = 0.01
+    frictionLoss = frictionFactor * (length * velocity ** 2) / (pipeDiameter * 2 * gravity)
+    # Need to add in Minor Losses
+    return frictionLoss
+
 def simulateDrain (vInitial, length, timeStep):
+    # Initial Conditions
     volume = vInitial
     time = 0
+    velocity = 0
     while volume >= 0:
-        print("Volume: ", round(volume, 3))
-        print("Time: ", round(time, 3))
         height = volume / tankArea + 0.02 + length / 150
-        losses = 0
+        # Calculates losses based on the velocity of the last time step
+        losses = findLosses(length, velocity)
         velocity = math.sqrt(2 * gravity * (height - losses))
         flowRate = pipeArea * velocity
         volume -= timeStep * flowRate
         time += timeStep
-    print("Model Done, Tank Drained in ", round(time, 3))
+    print("Model Done (Length ", length, "), Tank Drained in ", round(time, 3), "s", datetime.timedelta(seconds = time))
 
 v1 = 0.36 * 0.32 * 0.08
 # Pipe lengths: 0.2, 0.3, 0.4, 0.6
-simulateDrain(v1, 0.2, 0.01)
+simulateDrain(v1, 0.2, 0.1)
+simulateDrain(v1, 0.3, 0.1)
+simulateDrain(v1, 0.4, 0.1)
+simulateDrain(v1, 0.6, 0.1)
