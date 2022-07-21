a817ffeb0dc0853f1030ed7d7f06441975d12f5b
Cormac Cureton <cormaccureton@gmail.com>
2022-07-21 15:58:39 -0400

    Add some plotting for velocity and volume

--- fluidSim.py
+++ fluidSim.py
@@ -1,5 +1,8 @@
 import math
 import datetime
+import matplotlib.pyplot as plt
+
+plt.style.use('_mpl-gallery')
 
 # All values in meters
 tankArea = 0.32 * 0.36
@@ -19,15 +22,35 @@ def simulateDrain (vInitial, length, timeStep):
     volume = vInitial
     time = 0
     velocity = 0
+    volumes = []
+    velocities = []
+    times = []
     while volume >= 0:
         height = volume / tankArea + 0.02 + length / 150
         # Calculates losses based on the velocity of the last time step
         losses = findLosses(length, velocity)
         velocity = math.sqrt(2 * gravity * (height - losses))
+        velocities.append(velocity)
         flowRate = pipeArea * velocity
         volume -= timeStep * flowRate
+        volumes.append(volume)
+        times.append(time)
         time += timeStep
     print("Model Done (Length ", length, "), Tank Drained in ", round(time, 3), "s", datetime.timedelta(seconds = time))
+    fig, ax = plt.subplots()
+    ax.scatter(times, volumes)
+    ax.set_xlabel("time (s)", fontsize=15)
+    ax.set_ylabel("volume ($m^3$)", fontsize=15)
+    ax.grid(True)
+    fig.tight_layout()
+    plt.show()
+    fig2, ax2 = plt.subplots()
+    ax2.scatter(times, velocities)
+    ax2.set_xlabel("time (s)", fontsize=15)
+    ax2.set_ylabel("velocity (m/s)", fontsize=15)
+    ax2.grid(True)
+    fig2.tight_layout()
+    plt.show()
 
 v1 = 0.36 * 0.32 * 0.08
 # Pipe lengths: 0.2, 0.3, 0.4, 0.6
