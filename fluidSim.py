b78661dcc541d97d0d71c91d8e6ade5afabac3cb
marthamatsui <55360376+marthamatsui@users.noreply.github.com>
2022-07-27 23:48:38 -0400

    add tee joint loss calculation (doesn't work)

--- fluidSim.py
+++ fluidSim.py
@@ -1,19 +1,20 @@
 import math
 import datetime
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-plt.style.use('_mpl-gallery')
+# plt.style.use('_mpl-gallery')
 
 # All values in meters
 tankArea = 0.32 * 0.36
 pipeDiameter = 0.00794
 pipeArea = math.pi / 4 * pipeDiameter ** 2
 gravity = 9.81
+teeJointLength = 0.04
 # Kinematic viscosity of water at 1atm, ~25C from textbook p. 809
 kinVisWater = 9.0e-7
 # print("kinVisWater", kinVisWater)
 
-def findLosses (length, velocity, height):
+def findLosses (length, velocity, height, teeJoint):
     if velocity == 0:
         velocity = 0.01
     # Laminar Friction Factor using 64/Re
@@ -27,27 +28,42 @@ def findLosses (length, velocity, height):
     frictionLoss = effectiveFrictionFactor * (length * velocity ** 2) / (pipeDiameter * 2 * gravity)
     
     # Calculate Minor Losses For Contraction and Expansion
-    contractionFactor = 0.21 * 0.42 * (1 - (pipeDiameter ** 2) / (height ** 2))
+    contractionFactor = 0.08 * 0.42 * (1 - (pipeDiameter ** 2) / (height ** 2))
 
     contractionLoss = contractionFactor * (velocity ** 2) / (2 * gravity)
 
     #to do: add tee joint friction and minor losses
-    #if (tJoint):
+    if teeJoint == True:
+        # tentative loss factor
+        teeJointLossFactor = 0.5
 
-    losses = frictionLoss + contractionLoss
+        teeFrictionLoss = (effectiveFrictionFactor * (teeJointLength * velocity ** 2) / (pipeDiameter * 2 * gravity))
+        print("Tee friction loss is ", teeFrictionLoss)
+        teeMinorLoss = teeJointLossFactor * (velocity ** 2) / (2 * gravity)
+        print("Tee minor loss is ", teeMinorLoss)
+        teeJointLoss = (teeFrictionLoss + teeMinorLoss) * 0.08
+        print("Tee joint loss is ", teeJointLoss)
+    else:
+        teeJointLoss = 0
+        
+    losses = frictionLoss + contractionLoss + teeJointLoss
+
+    # print("Losses are: ", losses)
+    # print("Height is ", height)
 
     return losses
 
 
-def simulateDrain (vInitial, length, timeStep):
+def simulateDrain (vInitial, length, timeStep, teeJoint):
     # Initial Conditions
     volume = vInitial
     time = 0
     velocity = 0
     while volume >= 0:
         height = volume / tankArea + 0.02 + length / 150
+        print("Height is", height)
         # Calculates losses based on the velocity of the last time step
-        losses = findLosses(length, velocity, height)
+        losses = findLosses(length, velocity, height, teeJoint)
         velocity = math.sqrt(2 * gravity * (height - losses))
         flowRate = pipeArea * velocity
         volume -= timeStep * flowRate
@@ -71,11 +87,12 @@ def simulateSimpleDrain(vInitial, length):
 
 v1 = 0.36 * 0.32 * 0.08
 # Pipe lengths: 0.2, 0.3, 0.4, 0.6
-simulateDrain(v1, 0.2, 0.1)
-simulateDrain(v1, 0.3, 0.1)
-simulateDrain(v1, 0.4, 0.1)
-simulateDrain(v1, 0.6, 0.1)
-simulateSimpleDrain(v1, 0.2)
-simulateSimpleDrain(v1, 0.3)
-simulateSimpleDrain(v1, 0.4)
-simulateSimpleDrain(v1, 0.6)
+# simulateDrain(v1, 0.2, 0.1, False)
+simulateDrain(v1, 0.6, 1, True)
+# simulateDrain(v1, 0.3, 0.1, False)
+# simulateDrain(v1, 0.4, 0.1, False)
+# simulateDrain(v1, 0.6, 1, False)
+# simulateSimpleDrain(v1, 0.2)
+# simulateSimpleDrain(v1, 0.3)
+# simulateSimpleDrain(v1, 0.4)
+# simulateSimpleDrain(v1, 0.6)
